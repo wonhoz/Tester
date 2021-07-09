83be6269da77c03b149356eb90eba28f1aafb61f
wonhokang <wonho.kang@smartcareworks.com>
2021-07-09 18:08:30 +0900

    GetAppName 프로젝트 추가

---
+++ Process/GetAppName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAppName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            Console.ReadLine();
+        }
+    }
+}
